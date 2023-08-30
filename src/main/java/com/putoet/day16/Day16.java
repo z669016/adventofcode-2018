38eb04d0df099f26ee879d5e8381923f99d6f9f6
Rene van Putten <z669016@gmail.com>
2023-08-30 22:37:45 +0300

    Introduced var, added timing, reduced scope where possible

--- src/main/java/com/putoet/day16/Day16.java
+++ src/main/java/com/putoet/day16/Day16.java
@@ -3,23 +3,23 @@ package com.putoet.day16;
 import com.putoet.device.InstructionFactory;
 import com.putoet.device.Regs;
 import com.putoet.resources.ResourceLines;
+import com.putoet.utils.Timer;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day16 {
     public static void main(String[] args) {
-        part1();
-        part2();
+        final var opcodeLines = ResourceLines.list("/day16.txt");
+        Timer.run(() -> part1(opcodeLines));
+        Timer.run(() -> part2(opcodeLines));
     }
 
-    private static void part1() {
-        final List<String> lines = ResourceLines.list("/day16.txt");
-        int threeOrMore = 0;
+    private static void part1(List<String> opcodeLines) {
+        var threeOrMore = 0;
 
-        for (int idx = 0; idx < lines.size(); idx += 4) {
-
-            final Set<String> instructionSet = inStructionSet(lines, idx);
+        for (var idx = 0; idx < opcodeLines.size(); idx += 4) {
+            final var instructionSet = inStructionSet(opcodeLines, idx);
             if (instructionSet.size() >= 3)
                 threeOrMore++;
         }
@@ -27,14 +27,13 @@ public class Day16 {
         System.out.println("how many samples in your puzzle input behave like three or more opcodes? " + threeOrMore);
     }
 
-    private static void part2() {
-        final List<String> opcodeLines = ResourceLines.list("/day16.txt");
-        final Map<Long, String> opcodes = opcodes(opcodeLines);
-        final InstructionFactory factory = new InstructionFactory(opcodes);
+    private static void part2(List<String> opcodeLines) {
+        final var opcodes = opcodes(opcodeLines);
+        final var factory = new InstructionFactory(opcodes);
 
-        final List<String> instructions = ResourceLines.list("/day16-2.txt");
-        Regs regs = new Regs();
-        for (String line : instructions) {
+        final var instructions = ResourceLines.list("/day16-2.txt");
+        var regs = new Regs();
+        for (var line : instructions) {
             regs = regs.apply(factory.of(InstructionMatcher.instruction(line)));
         }
 
@@ -42,14 +41,14 @@ public class Day16 {
     }
 
     private static Map<Long, String> opcodes(List<String> lines) {
-        final Map<Long, Set<String>> opcodes = new HashMap<>();
+        final var opcodes = new HashMap<Long, Set<String>>();
 
-        for (int idx = 0; idx < lines.size(); idx += 4) {
-            long[] before = InstructionMatcher.before(lines.get(idx));
-            long[] instruction = InstructionMatcher.instruction(lines.get(idx +1));
-            long[] after = InstructionMatcher.after(lines.get(idx +2));
+        for (var idx = 0; idx < lines.size(); idx += 4) {
+            final var before = InstructionMatcher.before(lines.get(idx));
+            final var instruction = InstructionMatcher.instruction(lines.get(idx + 1));
+            final var after = InstructionMatcher.after(lines.get(idx + 2));
 
-            final Set<String> instructionSet = InstructionMatcher.match(before, after, instruction);
+            final var instructionSet = InstructionMatcher.match(before, after, instruction);
             if (!opcodes.containsKey(instruction[0])) {
                 opcodes.put(instruction[0], instructionSet);
             } else {
@@ -61,10 +60,10 @@ public class Day16 {
     }
 
     private static Map<Long, String> reduce(Map<Long, Set<String>> opcodes) {
-        Set<String> singles = singles(opcodes);
+        var singles = singles(opcodes);
         while (singles.size() < 16) {
-            for (Long key : opcodes.keySet()) {
-                final Set<String> values = opcodes.get(key);
+            for (var key : opcodes.keySet()) {
+                final var values = opcodes.get(key);
                 if (values.size() > 1)
                     values.removeAll(singles);
             }
@@ -77,17 +76,16 @@ public class Day16 {
     }
 
     private static Set<String> singles(Map<Long, Set<String>> opcodes) {
-        Set<String> singles = opcodes.values().stream()
+        return opcodes.values().stream()
                 .filter(value -> value.size() == 1)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toSet());
-        return singles;
     }
 
     private static Set<String> inStructionSet(List<String> lines, int idx) {
-        long[] before = InstructionMatcher.before(lines.get(idx));
-        long[] instruction = InstructionMatcher.instruction(lines.get(idx +1));
-        long[] after = InstructionMatcher.after(lines.get(idx +2));
+        final var before = InstructionMatcher.before(lines.get(idx));
+        final var instruction = InstructionMatcher.instruction(lines.get(idx + 1));
+        final var after = InstructionMatcher.after(lines.get(idx + 2));
 
         return InstructionMatcher.match(before, after, instruction);
     }
--- src/main/java/com/putoet/day16/InstructionMatcher.java
+++ src/main/java/com/putoet/day16/InstructionMatcher.java
@@ -2,49 +2,49 @@ package com.putoet.day16;
 
 import com.putoet.device.Instruction;
 import com.putoet.device.Regs;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class InstructionMatcher {
-    private static final Pattern BEFORE = Pattern.compile("Before: \\[(\\d), (\\d), (\\d), (\\d)\\]");
-    private static final Pattern AFTER = Pattern.compile("After: +\\[(\\d), (\\d), (\\d), (\\d)\\]");
+class InstructionMatcher {
+    private static final Pattern BEFORE = Pattern.compile("Before: \\[(\\d), (\\d), (\\d), (\\d)]");
+    private static final Pattern AFTER = Pattern.compile("After: +\\[(\\d), (\\d), (\\d), (\\d)]");
     private static final Pattern INSTR = Pattern.compile("(\\d+) (\\d) (\\d) (\\d)");
 
     public static Set<String> match(long[] b, long[] a, long[] i) {
         assert b.length == 4 && a.length == 4 && i.length == 4;
 
-        final Regs before = new Regs(b);
-        final Regs after = new Regs(a);
+        final var before = new Regs(b);
+        final var after = new Regs(a);
 
-        final List<Instruction> instructionSet = Instruction.instructionSet(i[0], i[1], i[2], i[3]);
-        return instructionSet.stream().sequential()
+        final var instructionSet = Instruction.instructionSet(i[0], i[1], i[2], i[3]);
+        return instructionSet.stream()
                 .filter(instruction -> before.apply(instruction).equals(after))
                 .map(Instruction::name)
                 .collect(Collectors.toSet());
     }
 
-    public static long[] before(String line) {
-        final Matcher matcher = BEFORE.matcher(line);
+    public static long[] before(@NotNull String line) {
+        final var matcher = BEFORE.matcher(line);
         if (matcher.matches())
             return getLongs(matcher);
 
         throw new IllegalArgumentException("Invalid before: " + line);
     }
 
-    public static long[] after(String line) {
-        final Matcher matcher = AFTER.matcher(line);
+    public static long[] after(@NotNull String line) {
+        final var matcher = AFTER.matcher(line);
         if (matcher.matches())
             return getLongs(matcher);
 
         throw new IllegalArgumentException("Invalid after: " + line);
     }
 
-    public static long[] instruction(String line) {
-        final Matcher matcher = INSTR.matcher(line);
+    public static long[] instruction(@NotNull String line) {
+        final var matcher = INSTR.matcher(line);
         if (matcher.matches())
             return getLongs(matcher);
 
@@ -52,9 +52,11 @@ public class InstructionMatcher {
     }
 
     private static long[] getLongs(Matcher matcher) {
-        return new long[]{Long.parseLong(matcher.group(1)),
+        return new long[]{
+                Long.parseLong(matcher.group(1)),
                 Long.parseLong(matcher.group(2)),
                 Long.parseLong(matcher.group(3)),
-                Long.parseLong(matcher.group(4))};
+                Long.parseLong(matcher.group(4))
+        };
     }
 }
